f3281147e8ff47189c087c94f4446dbc59de9dc8
bakharevd <bakharevd@users.noreply.github.com>
2024-10-08 05:26:50 +0000

    deploy: 4e23679f69e7dc84ee23b641c9f3d66f81858c9c

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('show-more').addEventListener('click', function() {
+    const extraContent = document.getElementById('extra-content');
+    if (extraContent.style.display === 'none') {
+        extraContent.style.display = 'block';
+        this.textContent = 'Show Less';
+    } else {
+        extraContent.style.display = 'none';
+        this.textContent = 'Show More';
+    }
+});
